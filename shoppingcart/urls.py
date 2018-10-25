2db286d42610b959a83bfec1f2a580ab09b7dd5a
Ban Vuong <vuongdieuban@gmail.com>
2018-10-24 18:59:36 -0600

    prototyping cart_update_view to add and remove product in cart

--- shoppingcart/urls.py
+++ shoppingcart/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import cart_home_view
+from .views import cart_home_view, cart_update_view
 
 app_name = 'cart'
 urlpatterns = [
     path('', cart_home_view, name='cart-home'),
+    path('update/', cart_update_view, name='cart-update')
 ]
--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from product.models import Product
 from .models import Cart
 # Create your views here.
@@ -6,6 +7,25 @@ from .models import Cart
 
 def cart_home_view(request):
     template_name = "shoppingcart/cart_home.html"
-    obj = Cart.objects.new_or_get(request)
+    cart_obj = Cart.objects.new_or_get(request)
+    cart_products = cart_obj.products.all()
+    total_price = 0
+    for product in cart_products:
+        total_price = total_price + product.price
+    cart_obj.total = total_price
+    cart_obj.save()
     context = {}
     return render(request, template_name, context)
+
+
+def cart_update_view(request):
+    template_name = "shoppingcart/cart_home.html"
+    product_obj = Product.objects.get(id=3)
+    cart_obj = Cart.objects.new_or_get(request)
+    cart_obj.products.add(product_obj)  # add data into ManytoMany field
+    if product_obj in cart_obj.products.all():
+        cart_obj.products.remove(product_obj)
+    else:
+        cart_obj.products.add(product_obj)
+    # return render(request, template_name, context)
+    return redirect("cart:cart-home")
